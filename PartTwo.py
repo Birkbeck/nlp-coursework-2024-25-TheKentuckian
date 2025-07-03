bcf1d8f6bdc7f4cd5447145a921624f7c8b8c170
Tony Anzelmo <tony.anzelmo@gmail.com>
2025-07-03 10:50:24 +0100

    Cleanup etl_csv

--- PartTwo.py
+++ PartTwo.py
@@ -9,16 +9,12 @@ from sklearn.svm import SVC
 csv_path = Path.cwd() / "p2-texts" / "hansard40000.csv"
 
 def etl_csv():
-    df = pd.read_csv(csv_path)
-    
-    df.loc[df['party'] == 'Labour (Co-op)', 'party'] = 'Labour'
-    
     major_parties = ['Labour','Conservative','Scottish National Party','Liberal Democrat']
+
+    df = pd.read_csv(csv_path)
+    df.loc[df['party'] == 'Labour (Co-op)', 'party'] = 'Labour'
     df.drop(df[~df['party'].isin(major_parties)].index, inplace=True)
-
     df.drop(df[df['speech_class'] != 'Speech'].index, inplace=True)
-    print(df['speech_class'].unique())
-
     df.drop(df[df['speech'].str.len() < 1000].index, inplace=True)
 
     return df
